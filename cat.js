a927de22af847e11e0646ab8b70205c28cc02d6b
Seunghun Lee <waydi1@gmail.com>
2016-11-20 22:37:19 -0800

    Update using mvc pattern

--- cat.js
+++ cat.js
@@ -1,52 +1,74 @@
-var cats = [
-  {'name' : 'Kitty',
-   'image' : 'https://c1.staticflickr.com/3/2079/2241767250_b29d0eef70_z.jpg?zz=1',
-   'count' : 0},
-  {'name' : 'Cutty',
-   'image' : 'https://c1.staticflickr.com/3/2884/11098807816_38e2595591_n.jpg',
-   'count' : 0},
-  {'name' : 'Judy',
-   'image' : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzEamUnQcCAS1UFqonKc6VKfZ0ykFbNFdlBw9iWoythOffJS29RZqhkMQ',
-   'count' : 0},
-  {'name' : 'Bella',
-   'image' : 'https://imgflip.com/s/meme/Cute-Cat.jpg',
-   'count' : 0},
-  {'name' : 'Choco',
-   'image' : 'https://s-media-cache-ak0.pinimg.com/736x/ba/54/54/ba54544039e1310da2bfc32881cbd00a.jpg',
-   'count' : 0}
-];
+$(function() {
 
-cat_list = document.getElementsByClassName('cat-list')[0];
-cat_clicker = document.getElementsByClassName('cat-clicker')[0];
+  var cats = [];
 
-for (var i = 0; i < cats.length; i++) {
-  elem = document.createElement('div');
-  elem.setAttribute('id', 'cat' + i);
-  elem.textContent = cats[i].name;
+  var model = {
+    add_cat: function(name, image) {
+      var cat = {
+        id: cats.length,
+        name: name,
+        image: image,
+        count: 0
+      };
 
-  elem.addEventListener('click', function(cat_num) {
-    return function() {
-      cat_name = document.createElement('h1');
-      cat_image = document.createElement('img');
-      cat_count = document.createElement('h3');
+      cats.push(cat);
+    },
 
-      cat_name.textContent = cats[cat_num].name;
-      cat_image.src = cats[cat_num].image;
-
-      cat_image.addEventListener('click', function(cat_num) {
-        return function() {
-          cats[cat_num].count += 1;
-          cat_count.textContent = cats[cat_num].count;
-        }
-      }(cat_num));
-
-      cat_count.textContent = cats[cat_num].count;
-      cat_clicker.innerHTML = '';
-      cat_clicker.appendChild(cat_name);
-      cat_clicker.appendChild(cat_image);
-      cat_clicker.appendChild(cat_count);
+    init: function() {
+      this.add_cat(
+        'Kitty',
+        'https://c1.staticflickr.com/3/2079/2241767250_b29d0eef70_z.jpg?zz=1'
+      );
+      this.add_cat(
+        'Cutty',
+        'https://c1.staticflickr.com/3/2884/11098807816_38e2595591_n.jpg'
+      );
+      this.add_cat(
+        'Judy',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzEamUnQcCAS1UFqonKc6VKfZ0ykFbNFdlBw9iWoythOffJS29RZqhkMQ'
+      );
+      this.add_cat(
+        'Bella',
+        'https://imgflip.com/s/meme/Cute-Cat.jpg'
+      );
+      this.add_cat(
+        'Choco',
+        'https://s-media-cache-ak0.pinimg.com/736x/ba/54/54/ba54544039e1310da2bfc32881cbd00a.jpg'
+      );
     }
-  }(i));
+  };
 
-  cat_list.appendChild(elem);
-}
+  var octopus = {
+    getCats: function () {
+      return cats;
+    },
+
+    init: function() {
+      model.init();
+      view.init();
+    }
+  };
+
+  var view = {
+    init: function() {
+      this.$cat_list = $('.cat-list');
+      this.cat_list_template = $('script[data-template="list-item"]').html();
+
+      this.render_list();
+    },
+
+    render_list: function() {
+      var list_template = this.cat_list_template;
+      var $cat_list = this.$cat_list;
+      $cat_list.html('');
+
+      octopus.getCats().forEach(function(cat) {
+        var this_template = list_template.replace(/{{id}}/g, cat.id);
+        this_template = this_template.replace(/{{cat_name}}/g, cat.name);
+        $cat_list.append(this_template);
+      });
+    }
+  };
+
+  octopus.init();
+});
