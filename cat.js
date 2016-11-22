69819d723680e1652d8e940a786aa8507d47c62b
Seunghun Lee <waydi1@gmail.com>
2016-11-21 16:49:04 -0800

    Simplify model

--- cat.js
+++ cat.js
@@ -1,59 +1,50 @@
 $(function() {
 
-  var cats = [];
-
   var model = {
-    add_cat: function(name, image) {
-      var cat = {
-        id: cats.length,
-        name: name,
-        image: image,
+    cats: [
+      {
+        name: 'Kitty',
+        image: 'https://c1.staticflickr.com/3/2079/2241767250_b29d0eef70_z.jpg?zz=1',
         count: 0
-      };
-
-      cats.push(cat);
-    },
-
-    init: function() {
-      this.add_cat(
-        'Kitty',
-        'https://c1.staticflickr.com/3/2079/2241767250_b29d0eef70_z.jpg?zz=1'
-      );
-      this.add_cat(
-        'Cutty',
-        'https://c1.staticflickr.com/3/2884/11098807816_38e2595591_n.jpg'
-      );
-      this.add_cat(
-        'Judy',
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzEamUnQcCAS1UFqonKc6VKfZ0ykFbNFdlBw9iWoythOffJS29RZqhkMQ'
-      );
-      this.add_cat(
-        'Bella',
-        'https://imgflip.com/s/meme/Cute-Cat.jpg'
-      );
-      this.add_cat(
-        'Choco',
-        'https://s-media-cache-ak0.pinimg.com/736x/ba/54/54/ba54544039e1310da2bfc32881cbd00a.jpg'
-      );
-    }
+      },
+      {
+        name: 'Cutty',
+        image: 'https://c1.staticflickr.com/3/2884/11098807816_38e2595591_n.jpg',
+        count: 0
+      },
+      {
+        name: 'Judy',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzEamUnQcCAS1UFqonKc6VKfZ0ykFbNFdlBw9iWoythOffJS29RZqhkMQ',
+        count: 0
+      },
+      {
+        name: 'Bella',
+        image: 'https://imgflip.com/s/meme/Cute-Cat.jpg',
+        count: 0
+      },
+      {
+        name: 'Choco',
+        image: 'https://s-media-cache-ak0.pinimg.com/736x/ba/54/54/ba54544039e1310da2bfc32881cbd00a.jpg',
+        count: 0
+      }
+    ]
   };
 
   var octopus = {
     getCats: function () {
-      return cats;
+      return model.cats;
     },
 
     getCat: function(cat_id) {
-      return cats[cat_id];
+      return model.cats[cat_id];
     },
 
     addCount: function(cat_id) {
-      cats[cat_id].count += 1;
+      model.cats[cat_id].count += 1;
       view.render_clicker(cat_id);
     },
 
     init: function() {
-      model.init();
       view.init();
     }
   };
@@ -78,13 +69,14 @@ $(function() {
     render_list: function() {
       var list_template = this.cat_list_template;
       var $cat_list = this.$cat_list;
+      var cats = octopus.getCats();
       $cat_list.html('');
 
-      octopus.getCats().forEach(function(cat) {
-        var this_template = list_template.replace(/{{id}}/g, cat.id);
-        this_template = this_template.replace(/{{cat_name}}/g, cat.name);
+      for (var i = 0; i < cats.length; i++) {
+        var this_template = list_template.replace(/{{id}}/g, i);
+        this_template = this_template.replace(/{{cat_name}}/g, cats[i].name);
         $cat_list.append(this_template);
-      });
+      }
     },
 
     render_clicker: function(cat_id) {
